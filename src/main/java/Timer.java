c3eb567f6813587a107815184a6052497e4322af
fabiolamartinez <fabiola.martinez@talan.com>
2019-10-15 14:31:08 -0400

    add timer

--- src/main/java/Timer.java
+++ src/main/java/Timer.java
@@ -1,2 +1,33 @@
-public class Timer {
+import java.util.concurrent.TimeUnit;
+
+public class Timer implements Runnable {
+    private int clock;
+    private int speed;
+    private int sleepTime;
+
+    Timer(int sleepTime, int speed){
+        this.clock = 0;
+        this.speed = sleepTime;
+        this.sleepTime = sleepTime;
+    }
+
+    public int getSleepTime() {
+        return sleepTime;
+    }
+
+    public void setSleepTime(int sleepTime) {
+        this.sleepTime = sleepTime;
+    }
+
+    public int addTime(int timeQty){
+        return this.clock + timeQty;
+    }
+
+    public void run() {
+        try {
+            Thread.sleep(this.sleepTime * this.speed);
+        } catch (InterruptedException e) {
+        }
+
+    }
 }
